7ee192392e980b07d54284f1707b644e1a9b68e8
maxelee <maxlee1993@berkeley.edu>
2020-09-11 15:17:05 -0700

    removing more big files

--- Lab_1/Lab1_data/Lab_1_DSB/capture.py
+++
@@ -1,6 +0,0 @@
-import sys
-import numpy as np
-import ugradio.pico as ugp
-
-def main(argv):
-	
--- Lab_1/Lab1_data/freq_res.py
+++
@@ -1,9 +0,0 @@
-import numpy as np
-import ugradio.pico as ugp
-import os
-import glob
-
-wf = ugp.capture_data(divisor=2, volt_range='2V')
-np.savez('/home/maxlee1993/Documents/Lab_1_freq_res/', wf)
-print('saved')
-print(sorted(glob.glob('/home/maxlee1993/Documents/Lab_1_freq_res/'+'*')))
